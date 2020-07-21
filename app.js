dfd9a307bff15efa9e2ffa3cd68372f139cd2fae
IT-eto-prosto <68605686+IT-eto-prosto@users.noreply.github.com>
2020-07-21 18:59:17 +0300

    Загрузил файлы

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const PORT = 5000
+const app = express()
+
+app.listen(PORT, () => {console.log(`Server has been started on port ${PORT}. Waiting for commands...`)})
+
+//Готово!
